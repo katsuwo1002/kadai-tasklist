5a053e7438aa8418f061fdb4fbd5af6d22f0c75e
katsuwo <ubuntu@ip-172-31-2-25.ap-northeast-1.compute.internal>
2021-08-02 06:59:05 +0000

    shuusei4

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:show, :edit]
-  #before_action :current_user, only: [:show, :edit, :destroy]
+  before_action :correct_user, only: [:show, :edit, :update, :destroy]
   
   def index
     @tasks = current_user.tasks.order(id: :desc)
@@ -49,10 +47,6 @@ class TasksController < ApplicationController
   
   private
   
-  def set_task
-    @task = Task.find(params[:id])
-  end
-  
   def task_params
     params.require(:task).permit(:content, :status)
   end
@@ -61,7 +55,6 @@ class TasksController < ApplicationController
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
-      #↑訂正前　redirect_to task_url
     end
   end
 end
